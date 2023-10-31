ed692394595403871a462ec12fb244727a034276
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-10-31 19:51:17 +0100

    update

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,11 +1,29 @@
 import { useAuth0 } from "@auth0/auth0-react";
+import { useEffect, useState } from "react";
 import Backdrop from "../components/common/Backdrop";
 
 const Profile = () => {
+  const [token, setToken] = useState<string | null>(null);
   const { user, isAuthenticated, isLoading, getAccessTokenSilently } =
     useAuth0();
 
   console.log("authenticated user", user);
+
+  useEffect(() => {
+    (async () => {
+      const accessToken = await getAccessTokenSilently();
+
+      console.log("token", accessToken);
+      setToken(accessToken);
+    })();
+  }, [isAuthenticated]);
+
+  console.log("state", token);
+
+  // const getToken = useCallback(async () => {
+  //   const accessToken = await getAccessTokenSilently();
+  //   return accessToken;
+  // }, [])
 
   if (isLoading) return <Backdrop />;
 
