20c51164125698aec5ac504cb63a74f2145c3124
Etumnu Prosper <johnikechukwu94@gmail.com>
2023-10-31 10:50:02 +0100

    Initialize react app

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className=" px-8 py-4 z-50 hidden font-sans  lg:flex shadow-sm  bg-white sticky top-0 justify-between items-center">
+      <h2 className="text-title logo  font-semibold">
+        𝕭𝖑𝖔𝖌<b className="text-primary">𝕿𝖗𝖊𝖓𝖉𝖘</b>
+      </h2>
+    </div>
+  );
+};
+
+export default Header;
